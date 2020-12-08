6ccb6a79b8a78e2dd5ff43a1c239d24556bfcb4a
Juniell <puchkina2000@mail.ru>
2020-12-09 02:20:06 +0300

    Added bound service

--- app/src/main/java/com/example/startedservicedownload/DownloadService.kt
+++ app/src/main/java/com/example/startedservicedownload/DownloadService.kt
@@ -1,11 +1,11 @@
 package com.example.startedservicedownload
 
+import android.annotation.SuppressLint
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.IBinder
+import android.os.*
 import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
@@ -15,34 +15,65 @@ import java.io.InputStream
 import java.net.URL
 import java.util.*
 
+private const val MSG = 1
+private const val URL = "url"
+private const val PATH = "path"
+
 class DownloadService : Service() {
+    private lateinit var mMessenger: Messenger
+
+    @SuppressLint("HandlerLeak")
+    inner class IncomingHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MSG -> runBlocking {
+                    val url = msg.data.getString(URL)!!
+                    val path = downloadCor(url)
+
+                    val m = Message.obtain(null, MSG, 0, 0)
+                    m.data = Bundle().apply {
+                        putString(PATH, path)
+                    }
+
+                    try {
+                        msg.replyTo.send(m)
+                    } catch (e: RemoteException) {
+                        e.printStackTrace()
+                    }
+                }
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         runBlocking {
             val url = intent?.extras?.getString("url")!!
-            downloadCor(url)
+            val path = downloadCor(url)
+            val int = Intent()
+            int.action = "com.example.startedservicedownload.PATH"
+            int.putExtra("path", path)
+            sendBroadcast(int)
         }
-        return super.onStartCommand(intent, flags, startId)
+        stopSelf(startId)
+        return START_STICKY
     }
 
-    private suspend fun downloadCor(url: String): Bitmap? =
+    private suspend fun downloadCor(url: String): String? =
         withContext(Dispatchers.IO) {
-            var mIcon11: Bitmap? = null
+            var path: String? = null
             var out: FileOutputStream? = null
             var inp: InputStream? = null
             try {
                 inp = URL(url).openStream()
-                mIcon11 = BitmapFactory.decodeStream(inp)
+                val mIcon = BitmapFactory.decodeStream(inp)
                 val uuid = UUID.randomUUID().toString().substring(0, 7)
                 val name = "$uuid.png"
-                out = openFileOutput(name, Context.MODE_PRIVATE)
-                mIcon11.compress(Bitmap.CompressFormat.PNG, 100, out)
 
-                val intent = Intent()
-                val path = "$filesDir/$name"
-                intent.action = "com.example.startedservicedownload.PATH"
-                intent.putExtra("path", path)
-                sendBroadcast(intent)
+                out = openFileOutput(name, MODE_PRIVATE)
+                mIcon.compress(Bitmap.CompressFormat.PNG, 100, out)
+                path = "$filesDir/$name"
+
                 Log.i("path", path)
             } catch (e: Exception) {
                 Log.e("Error", e.message!!)
@@ -51,8 +82,11 @@ class DownloadService : Service() {
                 inp?.close()
                 out?.close()
             }
-            return@withContext mIcon11
+            return@withContext path
         }
 
-    override fun onBind(intent: Intent?): IBinder? = null
+        override fun onBind(intent: Intent?): IBinder? {
+        mMessenger = Messenger(IncomingHandler())
+        return mMessenger.binder
+    }
 }
--- app/src/main/java/com/example/startedservicedownload/MainActivity.kt
+++ app/src/main/java/com/example/startedservicedownload/MainActivity.kt
@@ -1,17 +1,57 @@
 package com.example.startedservicedownload
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
+import android.content.ServiceConnection
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val MSG = 1
+private const val URL = "url"
+private const val PATH = "path"
+
 class MainActivity : AppCompatActivity() {
+    private var mService: Messenger? = null
+    private var bound = false
+    private val replyMess = Messenger(HandlerReplyMsg())
+
+    private val mConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            mService = Messenger(service)
+            bound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mService = null
+            bound = false
+        }
+    }
+
+    inner class HandlerReplyMsg : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MSG -> {
+                    val path = msg.data.getString(PATH)
+                    changePath(path)
+                }
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+
+    fun changePath(path: String?) {
+        if (path != null)
+            path_view.text = path
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val defaultAddress = "https://clck.ru/SMLw2"
 
-        save_btn.setOnClickListener {
+        started_btn.setOnClickListener {
             val intent = Intent(this, DownloadService::class.java)
             intent.putExtra("url",
                     if (url_text.text.isNotEmpty())
@@ -20,6 +60,45 @@ class MainActivity : AppCompatActivity() {
                         defaultAddress
             )
             startService(intent)
+        }
+
+        bound_btn.setOnClickListener {
+            if (!bound)
+                return@setOnClickListener
+
+            val msg = Message.obtain(null, MSG, 0, 0)
+            msg.replyTo = replyMess
+            msg.data = Bundle().apply {
+                putString(
+                    URL,
+                    if (url_text.text.isNotEmpty())
+                        url_text.text.toString()
+                    else
+                        defaultAddress
+                )
+            }
+
+            try {
+                mService?.send(msg)
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val intent = Intent(this, DownloadService::class.java)
+        val bind = bindService(intent, mConnection, Context.BIND_AUTO_CREATE)
+        if (!bind)
+            unbindService(mConnection)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (bound) {
+            unbindService(mConnection)
+            bound = false
         }
     }
 
